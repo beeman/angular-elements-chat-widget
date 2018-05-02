0f3c48564182d75d5ec600df05349f4c8753968b
Bram Borggreve <borggreve@gmail.com>
2018-05-02 01:55:55 -0500

    Improve layout on small screens

--- src/app/chat/chat-input/chat-input.component.ts
+++ src/app/chat/chat-input/chat-input.component.ts
@@ -10,42 +10,7 @@ import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, 
     </button>
   `,
   encapsulation: ViewEncapsulation.None,
-  styles: [`
-    .chat-input-text {
-      margin: 10px 0 0 10px;
-      height: 25px;
-      width: 70%;
-      border: 0;
-      resize: none;
-      border: none;
-      overflow: auto;
-      outline: none;
-      box-shadow: none;
-      font-size: 14px;
-      background-color: inherit;
-      color: inherit;
-    }
-
-    .chat-input-text::-webkit-input-placeholder {
-      color: inherit;
-    }
-    .chat-input-text::-moz-placeholder {
-      color: inherit;
-    }
-    .chat-input-text::-ms-input-placeholder {
-      color: inherit;
-    }
-
-    .chat-input-submit {
-      margin: 9px 3px 0px;
-      float: right;
-      background-color: inherit;
-      color: inherit;
-      font-size: 24px;
-      border: 0;
-      outline: none;
-    }
-  `],
+  styleUrls: ['./chat-input.component.css'],
 })
 export class ChatInputComponent implements OnInit {
   @Input() public buttonText = '↩︎'
